a6b32bea7b94d2e4810485189b375c9cc28c8bcd
guojie <273257924@qq.com>
2018-07-30 19:47:52 +0800

    事务annotation注解方式

--- oa/src/main/java/org/fkjava/oa/admin/identity/service/impl/IdentityServiceImpl.java
+++ oa/src/main/java/org/fkjava/oa/admin/identity/service/impl/IdentityServiceImpl.java
@@ -9,8 +9,10 @@ import org.fkjava.oa.admin.identity.service.IdentityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service(value="identityService")
+@Transactional(readOnly=false, rollbackFor=RuntimeException.class)
 public class IdentityServiceImpl implements IdentityService {
 	
 	@Autowired(required=true)
@@ -22,6 +24,7 @@ public class IdentityServiceImpl implements IdentityService {
 	private UserDao userDao;
 	
 	@Override
+	@Transactional(readOnly=true)
 	public List<Job> getJobs() {
 		System.out.println(userDao);
 		return jobDao.find(Job.class);
